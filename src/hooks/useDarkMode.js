544f3b44154a5264cf16c06d344a0296aca0fa20
alexis-anderson <aanderson9313@gmail.com>
2020-01-21 21:47:02 -0500

    added hooks

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,26 @@
+import {useEffect} from 'react';
+import useLocalStorage from '../hooks/useLocalStorage';
+
+
+ export const useDarkMode = () => {
+    const [enabled, setEnabled] = useLocalStorage(key, initialValue)
+
+    useEffect(() {
+        if (enabled === true) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    }, [enabled]); 
+    
+
+    const toggleDarkMode = e => {
+        e.preventDefault();
+
+        return enabled ? setEnabled(false) : enabled(true);
+    };
+
+
+    return [ enabled, setEnabled, toggleDarkMode];
+
+};
